9dfaca0da450db4804224a65410a41882f85f4e4
leo <nascimentoleo899@gmail.com>
2023-10-18 20:31:24 -0300

    first commit

---
+++ src/main/java/tech/leondev/wakishop/WakishopApplication.java
@@ -0,0 +1,13 @@
+package tech.leondev.wakishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakishopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/leondev/wakishop/WakishopApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.leondev.wakishop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WakishopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
