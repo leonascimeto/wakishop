63ba2f19ebd02fd17bb70292800a79008fd3d56a
leo <nascimentoleo899@gmail.com>
2023-10-21 21:32:18 -0300

    refactor price for BigDecimal

--- src/main/java/tech/leondev/wakishop/product/application/api/ProductRequestDTO.java
+++ src/main/java/tech/leondev/wakishop/product/application/api/ProductRequestDTO.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
+import java.math.BigDecimal;
+
 @Value
 public class ProductRequestDTO {
     @NotBlank
     private String name;
     @NotNull
-    private double price;
+    private BigDecimal price;
 }
